49e14a0ca961bdadb4a3903003be0a1e6245b027
Arturo <arturo.isidro@accenture.com>
2024-07-10 09:53:13 +0200

    first component in react js

---
+++ proyecto1/counter-app/src/FirstApp.jsx
@@ -0,0 +1,19 @@
+/*
+
+function App(){
+    //document.createElement ...
+    return <h1>Hola mundo</h1>;
+}
+
+export default App; // cuando tiene default en el 
+fichero destino no se pone {}, pero si no tiene default
+se tiene que poner {}
+
+*/
+
+export const FirstApp = () =>{
+
+    return (
+        <h1>Hola mundo esta es mi first app</h1>
+    );
+}
---
+++ proyecto1/counter-app/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { FirstApp }  from './FirstApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+    <FirstApp/>
+</React.StrictMode>);
